b828cdd6e989c97c84c47087588cec96ec782f0e
Lautaro Tallarico <lauta.tallarico@gmail.com>
2022-07-13 19:55:22 -0300

    Segundo desafio

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
+
     <div className="App">
-      <h1>Hola mundo!</h1>
+      <NavBar />
+      <h1>Mi tienda de react</h1>
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,21 @@
+
+const NavBar = () => {
+    return (
+        <div>
+            <nav>
+                <div class="nav-wrapper">
+                    <a href="" class="brand-logo">
+                        <img src="./assets/Hands-Hand-icon.png" alt="logo de tienda con forma de mano" />
+                    </a>
+                    <ul id="nav-mobile" class="right hide-on-med-and-down">
+                        <li><a href="sass.html">Productos</a></li>
+                        <li><a href="badges.html">Quienes somos?</a></li>
+                        <li><a href="collapsible.html">Contacto</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default NavBar
