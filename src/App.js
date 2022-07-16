2efc89934fa8658ecb97362016cbac643e42e2a7
Lautaro Tallarico <lauta.tallarico@gmail.com>
2022-07-16 17:41:28 -0300

    Correcciones 3er entrega

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 
 function App() {
   return (
 
     <div className="App">
       <NavBar />
+      <div>
+        <ItemListContainer />
+      </div>
+
     </div>
   );
 }
---
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -0,0 +1,11 @@
+
+
+const ItemListContainer = ({section}) => {
+    return (
+       <div className="list-products">
+            <h2>{section}</h2>
+       </div>
+    )
+}
+
+export default ItemListContainer
