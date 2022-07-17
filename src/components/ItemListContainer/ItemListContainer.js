3d6c4108bc078e8dd9bb65199de7180c1963d719
Lautaro Tallarico <lauta.tallarico@gmail.com>
2022-07-17 14:45:26 -0300

    Correcciones marcadas por tutor

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -4,6 +4,7 @@ const ItemListContainer = ({section}) => {
     return (
        <div className="list-products">
             <h2>{section}</h2>
+            <p>Aca voy a poner cosas :P</p>
        </div>
     )
 }
