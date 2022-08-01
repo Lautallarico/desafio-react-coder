3b3bd4ab98ed8da9bab3dfbc7c14740751f4d1ab
Lautaro Tallarico <lauta.tallarico@gmail.com>
2022-07-31 23:13:25 -0300

    retoques css

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -8,6 +8,27 @@ const ItemDetail = ({ data }) => {
 
     return (
         <div className="item-detail">
+            <div>
+                <img src={`/assets/${image}`} alt="imagen de producto" />
+            </div>
+            <div>
+                <p>{title}</p>
+                <p>{hand}</p>
+                <p>{description}</p>
+                <ItemCount stock={stock} initial={1} />
+                <p>$ {price}</p>
+                <button>Comprar</button>
+            </div>
+
+        </div>
+    )
+}
+
+export default ItemDetail
+
+
+/* 
+<div className="item-detail">
             <img src={`/assets/${image}`} alt="imagen de producto" />
             <p>{title}</p>
             <p>{hand}</p>
@@ -15,8 +36,5 @@ const ItemDetail = ({ data }) => {
             <ItemCount stock={stock} initial={1} />
             <p>$ {price}</p>
             <button>Comprar</button>
-        </div>
-    )
-}
-
-export default ItemDetail
+</div> 
+*/
